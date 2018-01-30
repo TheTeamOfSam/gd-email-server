520c79dfe75c691e3fe200b148ab2a42f581ba9e
sam199510 <273045049@qq.com>
2018-01-30 14:04:33 +0800

    邮件服务内容改进 新增@EnableDiscoveryClient注解以启用发现客户端服务

--- src/main/java/com/sam/graduation/design/gdemailserver/GdEmailServerApplication.java
+++ src/main/java/com/sam/graduation/design/gdemailserver/GdEmailServerApplication.java
@@ -2,9 +2,11 @@ package com.sam.graduation.design.gdemailserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GdEmailServerApplication {
 
