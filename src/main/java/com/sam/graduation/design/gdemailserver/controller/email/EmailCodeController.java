6f7521b733ad8e0e2959d88843a98748ebde64dd
sam199510 <273045049@qq.com>
2018-03-11 21:04:53 +0800

    邮箱验证服务新增对空的判断
    邮箱验证服务新增正则对邮箱格式进行判断

--- src/main/java/com/sam/graduation/design/gdemailserver/controller/email/EmailCodeController.java
+++ src/main/java/com/sam/graduation/design/gdemailserver/controller/email/EmailCodeController.java
@@ -5,6 +5,7 @@ import com.sam.graduation.design.gdemailserver.controller.dto.EmailResponseDto;
 import com.sam.graduation.design.gdemailserver.service.email.EmailCodeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,21 +21,63 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("邮箱验证码发送及验证类")
 public class EmailCodeController extends BaseController {
 
-	@Autowired
-	private EmailCodeService emailCodeService;
+    @Autowired
+    private EmailCodeService emailCodeService;
 
-	@ApiOperation("邮箱验证码发送接口")
-	@RequestMapping(value = "/email/code/@send", method = RequestMethod.POST)
-	public EmailResponseDto emailCodeSend(@RequestParam("email") String email) {
-		EmailResponseDto dto = this.emailCodeService.sendEmailCode(email);
-		return dto;
-	}
+    @ApiOperation("邮箱验证码发送接口")
+    @RequestMapping(value = "/email/code/@send", method = RequestMethod.POST)
+    public EmailResponseDto emailCodeSend(
+            @RequestParam(value = "email", required = false) String email
+    ) {
+        if (StringUtils.isBlank(email)) {
+            EmailResponseDto dto = new EmailResponseDto();
+            dto.setCode(null);
+            dto.setFeedbackMessage("亲，请输入邮箱！");
+            dto.setSuccess(false);
+            return dto;
+        }
+        String emailRegex = "[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?";
+        if (!email.matches(emailRegex)) {
+            EmailResponseDto dto = new EmailResponseDto();
+            dto.setCode(null);
+            dto.setFeedbackMessage("亲，邮箱格式不正确！");
+            dto.setSuccess(false);
+            return dto;
+        }
+        EmailResponseDto dto = this.emailCodeService.sendEmailCode(email);
+        return dto;
+    }
 
-	@ApiOperation("短信验证码验证接口")
-	@RequestMapping(value = "/email/code/@check", method = RequestMethod.POST)
-	public EmailResponseDto emailCodeCheck(@RequestParam("email") String email, @RequestParam("code") String code) {
-		EmailResponseDto dto = this.emailCodeService.checkEmailCode(email, code);
-		return dto;
-	}
+    @ApiOperation("短信验证码验证接口")
+    @RequestMapping(value = "/email/code/@check", method = RequestMethod.POST)
+    public EmailResponseDto emailCodeCheck(
+            @RequestParam(value = "email", required = false) String email,
+            @RequestParam(value = "code", required = false) String code
+    ) {
+        if (StringUtils.isBlank(email)) {
+            EmailResponseDto dto = new EmailResponseDto();
+            dto.setCode(null);
+            dto.setFeedbackMessage("亲，请输入邮箱");
+            dto.setSuccess(false);
+            return dto;
+        }
+        if (StringUtils.isBlank(code)) {
+            EmailResponseDto dto = new EmailResponseDto();
+            dto.setCode(null);
+            dto.setFeedbackMessage("亲，请输入验证码！");
+            dto.setSuccess(false);
+            return dto;
+        }
+        String emailRegex = "[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?";
+        if (!email.matches(emailRegex)) {
+            EmailResponseDto dto = new EmailResponseDto();
+            dto.setCode(null);
+            dto.setFeedbackMessage("亲，邮箱格式不正确");
+            dto.setSuccess(false);
+            return dto;
+        }
+        EmailResponseDto dto = this.emailCodeService.checkEmailCode(email, code);
+        return dto;
+    }
 
 }
